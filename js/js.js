c2c8ee61f1b8f2aff0332a93c6351334459d4515
jesuslujan98 <51263684+jesuslujan98@users.noreply.github.com>
2019-10-16 10:45:17 -0500

    Agregando funciones a JS

--- js/js.js
+++ js/js.js
@@ -75,3 +75,122 @@ function eliminar(id){
 }
 
 
+function iniciarSesion(a){
+	if(a==0){
+		var user = $("#txtusuario").val();
+		$.ajax({
+		  url: 'login.php',
+		  type: 'post',
+		  data: {"txtuser":user, "a":1},
+		  success: function( data ){
+			if(data == 1){
+				window.location.href="index_pass.php";
+			}
+			else{
+				Swal.fire({
+					title: '¡Eror al iniciar sesion!',
+					text: "Revisa bien tus datos",
+					type: 'error',
+					showCancelButton: false,
+					confirmButtonColor: '#FF4242',
+					//cancelButtonColor: '#d33',
+					confirmButtonText: 'Volver a intentar'
+				  }).then((result) => {
+					if (result.value) {
+					  location.reload();
+					}
+					else{
+					  //window.location.href="login.html";
+					}
+				  })  
+			}
+		  },
+		  error: function( jqXhr, textStatus, error ){
+			  console.log( error );
+		  }
+	  });
+	}
+	else{
+		var pass = $("#txtpassw").val();
+		$.ajax({
+		  url: 'login.php',
+		  type: 'post',
+		  data: {"txtpass":pass,"a":2},
+		  success: function( data ){
+			if(data == 1){
+				Swal.fire({
+					title: '¡Bienvenido!',
+					text: "Disfruta de tu estadia",
+					type: 'success',
+					showCancelButton: false,
+					confirmButtonColor: '#3085d6',
+					//cancelButtonColor: '#d33',
+					confirmButtonText: 'Aceptar'
+				  }).then((result) => {
+					if (result.value) {
+					  window.location.href="persona.php";
+					}
+					else{
+					  window.location.href="persona.php";
+					}
+				  }) 
+			}
+			else{
+				Swal.fire({
+					title: '¡Eror al iniciar sesion!',
+					text: "Revisa bien tus datos",
+					type: 'error',
+					showCancelButton: false,
+					confirmButtonColor: '#FF4242',
+					//cancelButtonColor: '#d33',
+					confirmButtonText: 'Volver a intentar'
+				  }).then((result) => {
+					if (result.value) {
+						//window.location.href="persona.php";
+					}
+					else{
+					  //window.location.href="login.html";
+					}
+				  })  
+			}
+		  },
+		  error: function( jqXhr, textStatus, error ){
+			  console.log( error );
+		  }
+	  });
+	}
+  }
+
+
+  function validar(){
+
+    var forms = document.getElementsByClassName('needs-validation');
+    var validation = Array.prototype.filter.call(forms, function(form) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+          console.log("Error al validar");
+          captcha();
+        }
+        else{
+          var response = grecaptcha.getResponse();
+          if(response.length !== 0){
+            registrar();
+          }    
+        }
+        form.classList.add('was-validated');
+        });
+  }
+
+  function captcha(){
+    setInterval(function(){
+      var response = grecaptcha.getResponse();
+      if(response.length == 0){
+        $("#msg-capt").show();
+      } else {
+        $("#msg-capt").hide();
+      }
+
+    },100);
+}
+
--- php/login.php
+++ php/login.php
@@ -19,4 +19,50 @@ if ($rs -> rowCount() == 1) {
     header("location: ../html/iniciarsesion.html");
 }
 
+
+/*
+
+if($a == 1){
+	$usuario = $_POST['txtuser'];
+	$sql = "SELECT * FROM usuario 
+	WHERE usuario='$usuario'";
+	$rs = $cnx->query($sql) or die($sql);
+	$cantreg = $rs->rowCount();		
+	if($cantreg==1) {
+		session_start();
+		$reg = $rs->fetchObject();
+		$_SESSION['usuario']=$reg->usuario;
+		echo 1;
+	}
+	else{
+		echo 2;
+	}
+	
+}
+else{
+	$passw = md5($_POST['txtpass']);	
+	session_start();
+	$usuario = $_SESSION['usuario'];
+	$sql = "SELECT * FROM usuario 
+	WHERE  password = '$passw' and usuario ='$usuario'";	
+	$rs = $cnx->query($sql) or die($sql);
+	$cantreg = $rs->rowCount();
+	if($cantreg==1) 
+	{
+		$reg = $rs->fetchObject();
+		$_SESSION['idusuario']=$reg->idusuario;
+		$_SESSION['usuario']=$reg->usuario;	
+		echo 1;				
+	}
+	else{
+		//session_start();  
+    	//unset($_SESSION['usuario']);
+		//session_destroy();
+		echo 2;
+	}
+}
+
+*/
+
+
  ?>
